24f47df685a6e0dd73b48e48f2b201a8f9ecdd53
Noya Onggud <yanuo44@gmail.com>
2021-11-30 00:06:25 -0500

    fixed the prompt issue

--- typewriter.js
+++ typewriter.js
@@ -4,15 +4,18 @@ const sentence = "hello there from lighthouse labs";
 const delayedCharAppearance = function(string, ms) {
 
   let delay = ms;
+  let counter=0;
 
   for (const char of string) {
     setTimeout(()=>{
       process.stdout.write(char);
     },delay);
     delay += 50;
-  }
-  // console.log("\n");
-  process.stdout.write("\n");
+    counter++;
+}
+    setTimeout(() => {
+        process.stdout.write("\n");
+    }, 50*(string.length));
 
 };
 delayedCharAppearance(sentence, 0);
